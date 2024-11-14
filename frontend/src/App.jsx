9ffaf7f0eb4962ab92ab7a4880952c496560043a
parnaud <arnaud.pfeillefort@ynov.com>
2024-11-14 12:25:34 +0100

    show API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,23 +9,43 @@ function App() {
 
   const formData = new FormData();
   formData.append("query", "star wars");
-  formData.append("page", 3);
+  formData.append("page", 1);
 
+  
   useEffect(() => {
     fetch('/api/api.php', {
       method: 'POST',
       body: formData,
     })
       .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Erreur:', error));
+      .then(data => setData(data["results"] ))
+      .catch(error => console.error('Erreur:', error));  
   }, [])
 
   return (
     <>
       <div>
           <h1>Données récupérées :</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+          
+          
+            <div style={{ display: "flex", gap:"20px"}} >{ 
+          
+            data ? data.map((movie, i) => {
+              return (
+                <>
+                  <div key={i}> 
+                      <h1> { movie["title"] } </h1> 
+                      <img src={ "https://image.tmdb.org/t/p/original/" + movie["poster_path"] } style={{ height: "1000px", width: "670px" }}/> 
+                  </div>
+                </>
+              )
+            })  : <h1> AHAAAAAAAAAAAAAAAA</h1>
+            
+            }</div>
+         
+
+          
+
         </div>
      
     </>
